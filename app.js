13af1564589756d8036bae8450acc958de84c7a3
Caadil123 <abdirahimomar147@gmail.com>
2024-12-01 20:46:20 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,93 @@
-// UI element 
+// UI elements
 
-let height = document.getElementById('Height'),
-   weight = document.getElementById('Weight'),
-   calbtn = document.getElementById('calbtn'),
-   result = document.getElementById('result');
+let dayinput = document.getElementById('day'),
+   monthinput = document.getElementById('month'),
+   yearinput = document.getElementById('Year');
+   btncal = document.querySelector('.btn'),
+   currentyear = new Date().getFullYear(),
+   Specificdate  = new Date('2024-02-17'),
+   DayOfMonth  = Specificdate.getDate();
 
-calbtn.addEventListener('click', ()=>{
-  if(height.value !== '' || weight.value !== ''){
-    if(calbtn.innerHTML === 'Calculate'){
-      // convert height into meters
+   let currentMonth = new Date().getMonth() +1;
 
-     height = parseInt(height.value) / 100;
-    let bmi = parseInt(weight.value) / (height * height);
-    let message = '';
-    // conditions..
-    if(bmi<=18.5){
-      message = 'Underwight'
-    }else if(bmi >18.5 && bmi<=24.5){
-      message = 'Normal';
-    }else if(bmi >24.5 && bmi <=30){
-      message = 'Overweight'
-    }else{
-      message = 'Obese';
-    }
-    result.innerHTML = message + ':'+bmi.toFixed(2);
-    calbtn.innerHTML = 'Reset'
-    }else if(calbtn.innerHTML === "Reset"){
-      height.value = '';
-      weight.value = '';
-      result.innerHTML = '';
-      location.reload();
-    }
-  }else{
-    alert('Please fill the fields');
+// input feild validations.
 
-  }
+dayinput.addEventListener('keyup', ()=>{
+   if(isNaN(dayinput.value) || parseInt(dayinput.value)<=0){
+      document.getElementById('validDay').innerHTML='Invalid Day!';
+      return false;
+   }else{
+      document.getElementById('validDay').innerHTML = '';
+   }
 })
+monthinput.addEventListener('keyup',() =>{
+   if(isNaN(monthinput.value) || parseInt(monthinput.value)<1 || parseInt(monthinput.value)>12){
+      document.getElementById('validmonth').innerHTML = 'Invalid Month';
+      return false;
+   }else{
+      document.getElementById('validmonth').innerHTML = '';
+   }
+})
+yearinput.addEventListener("keyup", () => {
+   if (
+     !(
+       Number.isInteger(parseInt(yearinput.value, 10)) &&
+       parseInt(yearinput.value, 10) >= 1000 &&
+       parseInt(yearinput.value, 10) <= 2024
+     )
+   ) {
+     document.getElementById("validyear").innerHTML =
+       "must be between 1000-2024";
+     return false;
+   } else {
+     document.getElementById("validyear").innerHTML = "";
+   }
+ });
+ 
+
+// create function that calculate age;
+
+function calculate(){
+   let inputDay = parseInt(dayinput.value);
+       inputmonth = parseInt(monthinput.value),
+      inputyear = parseInt(yearinput.value);
+
+   // check all fields are empty 
+   if(isNaN(inputDay) || isNaN(inputmonth) || isNaN(inputyear)){
+      document.getElementById('validday').innerHTML = 'field this fields';
+      document.getElementById('validmonth').innerHTML = 'field this fields';
+      document.getElementById('validyear').innerHTML = 'field this fields';
+      return false;
+   }
+   // calucalte age
+   let ageYears = currentyear - inputyear,
+      ageMonths = currentMonth - inputmonth,
+      ageDays= DayOfMonth - inputDay;
+
+      if(currentMonth < inputmonth){
+         ageMonths+=12;
+         ageYears-=1;
+      }
+      if(ageDays < 0){
+         ageMonths--;
+         ageDays+= new Date(currentyear, currentMonth - 1, 0).getDate();
+      }else{
+         ageDays = ageDays;
+      }
+
+      if(ageMonths < 0){
+         ageYears--;
+         ageMonths+=12;
+      }
+      if(ageYears<0){
+         alert('this birth is not exist');
+         return false;
+      }
+      document.getElementById('yearresult').innerHTML = ageYears;
+      document.getElementById('monthresult').innerHTML = ageMonths;
+      document.getElementById('dayresult').innerHTML = ageDays;
+}
+
+//  add event listener to button;
+
+btncal.addEventListener('click', calculate);
